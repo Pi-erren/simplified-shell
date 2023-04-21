431a1769fff2661eae8c5a27d527c211a74e479e
Pi-erren <pneveu937@gmail.com>
2023-04-21 09:54:21 +0200

    v1 exit and interface

--- dash.c
+++ dash.c
@@ -1,13 +1,34 @@
 #include <stdio.h>
 #include "read_input.h"
+#include <stdlib.h>
 
-int main(void){
+int main(void)
+{
 
-    // Read the input of the user
-    char input = malloc(read_input());
+    // Useful variables
+    char **words = malloc(50 * sizeof(char *));
+    char *path = malloc(50 * sizeof(char));
+    size_t bufsize = 30;
+    char *dash = "dash> ";
 
-    while(){
+    // Programme
+    while (1)
+    {
 
+        // Read user input
+        printf("%s", dash);
+        getline(&line, &bufsize, stdin);
+
+        // Exit command
+        if (line == "exit")
+        {
+            free(line);
+            exit(0);
+        }
+        // else if ()
+        // {
+        // }
+
+        printf("%s", line);
     }
-    return 0;
 }
--- read_input.h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int read_input(){
-    
-    // Declare buffer for the input line
-    char* line = malloc(30* sizeof(char));
-    
-
-}
